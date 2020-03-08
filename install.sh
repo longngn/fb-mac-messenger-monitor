c3b51c9fda10a3c66d4952872d75b6169481a7c8
Nguyen Le Vu Long <vulongvn98@gmail.com>
2020-03-08 18:31:53 +0700

    Update

--- install.sh
+++ install.sh
@@ -2,9 +2,16 @@
 
 set -e
 
+SERVICE_PATH=$HOME/Library/LaunchAgents/local.fb.mac.messenger.monitor.plist
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "Install monitor script"
 cp -f $here/fb-mac-messenger-monitor.sh /usr/local/bin/
 chmod +x /usr/local/bin/fb-mac-messenger-monitor.sh
-cp -f $here/local.fb.mac.messenger.monitor.plist $HOME/Library/LaunchAgents/
-launchctl load $HOME/Library/LaunchAgents/local.fb.mac.messenger.monitor.plist
+
+echo "Install launchd service"
+launchctl unload $SERVICE_PATH
+cp -f $here/local.fb.mac.messenger.monitor.plist $SERVICE_PATH
+launchctl load $SERVICE_PATH
+
+echo "Done"
