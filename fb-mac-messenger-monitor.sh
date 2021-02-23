36e52f324fc0acc3907615ec55a7d9838e61a205
Nguyen Le Vu Long <vulongvn98@gmail.com>
2021-02-24 01:01:20 +0700

    fix: sum ram with there's multiple processes

--- fb-mac-messenger-monitor.sh
+++ fb-mac-messenger-monitor.sh
@@ -3,7 +3,7 @@
 APP_PATH=/Applications/Messenger.app
 RAM_LIMIT_MB=500
 
-ram_usage=$(ps aux | grep $APP_PATH | grep -v grep | awk '{ print $6 }')
+ram_usage=$(ps aux | grep $APP_PATH | grep -v grep | awk '{ print $6 }' | paste -sd+ - | bc)
 if [ $ram_usage -ge $((RAM_LIMIT_MB*1024)) ]; then
   pid=$(ps aux | grep $APP_PATH | grep -v grep | awk '{ print $2 }')
   kill $pid
